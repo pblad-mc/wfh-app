2c37359ee67701ee0d81ca24c6a36079d69e6cca
hunter jenkins <hjenkins@mastercontrol.com>
2018-05-23 16:17:57 -0600

    Started the UI with alex stevens. We have 3 different pages all routing to eachother properly. The foundation of the web page has been created.

--- wfh/src/app/app.module.ts
+++ wfh/src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { LoginComponent } from './login/login.component';
+import { NonAdminComponent } from './non-admin/non-admin.component';
+import { AdminComponent } from './admin/admin.component';
+
+import { RouterModule, Router } from '@angular/router';
+
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ //"Now you can use the login tag that is referenced in the html"
+    AppComponent,
+    LoginComponent,
+    NonAdminComponent,
+    AdminComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot ([
+      { path: 'toNonAdmin', component: NonAdminComponent },
+      { path: 'toAdmin', component: AdminComponent },
+      { path: 'toLogin', component: LoginComponent },
+      { path: '', redirectTo: 'toLogin', pathMatch: 'full'},
+      { path: '**', redirectTo: 'toLogin', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ wfh/src/app/non-admin/non-admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: 'non-admin.component.html',
+    styleUrls: ['non-admin.component.css']
+})
+
+export class NonAdminComponent {
+
+}
