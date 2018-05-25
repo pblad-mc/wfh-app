c539283ad5171378f2c8652dd1077fed25063611
alex stevens <astevens@mastercontrol.com>
2018-05-25 08:51:28 -0600

    component functionality

--- wfh/src/app/login/login.component.ts
+++ wfh/src/app/login/login.component.ts
@@ -7,17 +7,34 @@ import { Component } from '@angular/core';
 })
 
 export class LoginComponent {
+    _username: string;
+    _password: string;
 
-    username: string = "";
-    password: string = "";
-
-    constructor()
-    {
-
+    get username(): string {
+        return this._username;
+    }
+    set username(value: string) {
+        this._username = value;
     }
 
-    onAdminClick()
-    {
-        debugger;
+    get password(): string {
+        return this._password;
+    }
+    set password(value: string) {
+        this._password = value;
+    }
+
+    employeeLogin(){
+        // TODO verify that it is a correct user
+        // Then go to the next page
+        console.log('you are logging in as a employee');
+        console.log("username: ", this.username)
+        console.log("password: ", this.password)
+    }
+
+    adminLogin(){
+        console.log('you are logging in as a administrator');
+        console.log("username: ", this.username)
+        console.log("password: ", this.password)
     }
 }
