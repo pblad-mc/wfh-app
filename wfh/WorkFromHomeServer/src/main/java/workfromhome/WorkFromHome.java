9934f338d116d1e23c52b121185c53057a8a695b
jjbradshaw <jjbradshaw@mastercontorl.com>
2018-05-30 13:51:32 -0600

    Initialize java project

---
+++ wfh/WorkFromHomeServer/src/main/java/workfromhome/WorkFromHome.java
@@ -0,0 +1,10 @@
+package main.java.workfromhome;
+
+public class WorkFromHome {
+
+	public static void main(String[] args) {
+		System.out.println("Starting up server...");
+		System.out.println("As of now this does nothing.");
+	}
+
+}
