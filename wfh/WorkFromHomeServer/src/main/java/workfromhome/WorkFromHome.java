2735e755d9b207b47ac339a818ab8f71680aa324
jjbradshaw <jjbradshaw@mastercontorl.com>
2018-05-30 14:15:52 -0600

    Add initial gradle configuration

--- wfh/WorkFromHomeServer/src/main/java/workfromhome/WorkFromHome.java
+++ wfh/WorkFromHomeServer/src/main/java/workfromhome/WorkFromHome.java
@@ -3,7 +3,7 @@ package main.java.workfromhome;
 public class WorkFromHome {
 
 	public static void main(String[] args) {
-		System.out.println("Starting up server...");
+		System.out.println("\nStarting up server...");
 		System.out.println("As of now this does nothing.");
 	}
 
